e85421f32f9690cb1f70781b6544b89244a98f6d
AmanRiat <37984325+AmanRiat@users.noreply.github.com>
2018-04-01 16:29:26 -0400

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prism;
+
+
+public class Rectangle {
+    // Method to calculate the area of a circle, does't return a value, i.e void
+	public void RectangleArea(double rect_length, double rect_width) {
+		double Rect_Area = (rect_length * rect_width);
+		System.out.println("Area of rectangle: " + Rect_Area);
+	}
+
+	// Method to calculate the perimeter of a circle, doesn't return a value, i.e void
+	public void RectanglePerimeter(double rect_length, double rect_width) {
+		double Rect_Perimeter = 2 * (rect_length * rect_width);
+		System.out.println("Perimeter of rectangle: " + Rect_Perimeter);
+	}
+    String ans2 = "yes"; 
+    
+}
